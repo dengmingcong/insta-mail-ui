088431f6c15ed967f424be75b02140bdad31cfe7
dengmingcong <mc.deng@outlook.com>
2024-12-12 08:44:21 +0800

    refactor: rename folder

--- src/app/mails/create/page.tsx
+++ src/app/mails/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import CIReportCreate from "@components/template/ci-report/create";
+import CIReportCreate from "@components/templates/ci-report/create";
 import { Autocomplete, Box, Button, Step, StepButton, Stepper, TextField, useMediaQuery, useTheme } from "@mui/material";
 import { Create, SaveButton, useAutocomplete } from "@refinedev/mui";
 import { useStepsForm } from "@refinedev/react-hook-form";
