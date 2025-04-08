b80761f5ea7e1750e74e874fa04049b02c4969f8
raigordeng <mc.deng@outlook.com>
2025-04-08 09:26:02 +0800

    chore: change api url port

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -2,6 +2,6 @@
 
 import dataProviderSimpleRest from "@refinedev/simple-rest";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "http://localhost:8000";
 
 export const dataProvider = dataProviderSimpleRest(API_URL);
