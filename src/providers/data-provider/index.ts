8cfff44c2793cf00089cd8cdbc59ff554b49ccfc
raigordeng <mc.deng@outlook.com>
2025-08-14 09:55:08 +0800

    style: format with biome

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -2,6 +2,7 @@
 
 import dataProviderSimpleRest from "@refinedev/simple-rest";
 
-const BACKEND_API_ORIGIN = process.env.BACKEND_API_ORIGIN || "http://localhost:8000";
+const BACKEND_API_ORIGIN =
+  process.env.BACKEND_API_ORIGIN || "http://localhost:8000";
 
 export const dataProvider = dataProviderSimpleRest(BACKEND_API_ORIGIN);
